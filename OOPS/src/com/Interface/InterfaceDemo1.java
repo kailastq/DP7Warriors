2013f2abfb819dea9bed0820a15c92204ca6eb4e
Asus <Asus@Kailash>
2023-10-10 09:47:08 +0530

    ok

--- OOPS/src/com/Interface/InterfaceDemo1.java
+++ OOPS/src/com/Interface/InterfaceDemo1.java
@@ -1,23 +1,23 @@
 package com.Interface;
 
-interface Bakery{
-	String bname="Cake World"; // it can be public static and final 
-	
-	 void bake();  // public abstract
+interface Bakery {
+	String bname = "Cake World"; // it can be public static and final
+
+	void bake(); // public abstract
 }
 
-class Cake implements Bakery{
+class Cake implements Bakery {
 	@Override
 	public void bake() {
 		System.out.println("We bake cakes: Butter, Vanilla, Chocolate");
 	}
 }
 
-class Biscuit implements Bakery{
+class Biscuit implements Bakery {
 	public void bake() {
 		System.out.println("We bake biscuit: Almond, Cashew");
 	}
-	
+
 	void miniOrder() {
 		System.out.println("Minimum order is 250gm");
 	}
@@ -27,20 +27,19 @@ public class InterfaceDemo1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		System.out.println("Bakery Name: "+Bakery.bname);
-		Cake c=new Cake();
+
+		System.out.println("Bakery Name: " + Bakery.bname);
+		Cake c = new Cake();
 		c.bake();
-		
-		Biscuit b=new Biscuit();
+
+		Biscuit b = new Biscuit();
 		b.bake();
 		b.miniOrder();
-		
-		Bakery b1;
-		
-		b1=new Biscuit();
+
+		Bakery b1=new Biscuit();
+//		b1 = new Biscuit();
 		b1.bake();
-	//	b1.miniOrder();
+		// b1.miniOrder();
 
 	}
 
