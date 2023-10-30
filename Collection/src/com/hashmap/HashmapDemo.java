1267232bfb14224e39223fb49fd34683364c0b67
Asus <Asus@Kailash>
2023-10-30 10:39:20 +0530

    Done

--- Collection/src/com/hashmap/HashmapDemo.java
+++ Collection/src/com/hashmap/HashmapDemo.java
@@ -2,7 +2,7 @@ package com.hashmap;
 
 import java.util.HashMap;
 
-public class HashmapDemp {
+public class HashmapDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
