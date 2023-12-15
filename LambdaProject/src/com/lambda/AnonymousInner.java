6e5c3fa2ac4036ee574f8882c3462ce2260a6e7a
Asus <Asus@Kailash>
2023-12-15 14:31:18 +0530

    ok

---
+++ LambdaProject/src/com/lambda/AnonymousInner.java
@@ -0,0 +1,32 @@
+package com.lambda;
+
+
+public class AnonymousInner {
+	
+	public static void main(String[] args) {
+		
+		Movable car = new Movable() {
+			
+			@Override
+			public void move() {
+				System.out.println("Car is moving");
+				
+			}
+		};
+		
+      Movable ball = new Movable() {
+			
+			@Override
+			public void move() {
+				System.out.println("Ball is rolling");
+				
+			}
+		};
+		
+		car.move();
+		ball.move();
+		
+	}
+
+
+}
