6323d687b57e7a63cf228967396e96571cda3c18
Asus <Asus@Kailash>
2024-01-03 18:47:24 +0530

    ok

--- String/src/com/stringLogical/DeleteSpacesString.java
+++ String/src/com/stringLogical/DeleteSpacesString.java
@@ -7,13 +7,13 @@ public class DeleteSpacesString {
 		{
 			str=str.replaceAll("  "," ");
 		}
-		System.out.println(str.replaceAll(" ", " "));
+		System.out.println(str);//.replaceAll(" ", " "));
 			
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String s="I am  Kailash   Patil";
+		String s="I am  Kailash                 Patil";
 		deleteSpaces(s);
 		
 
